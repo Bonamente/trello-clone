0d3af29aff1f95824b3ca34ed2df18d4397315dd
Bonamente <56506035@protonmail.com>
2022-07-17 12:52:45 +0500

    feat: add moveItem helper function

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -3,5 +3,18 @@ type Item = {
 }
 
 export const findItemIndexById = <TItem extends Item>(
-  items: TItem[],
-  id: string): number => (items.findIndex((item: TItem) => item.id === id));
+  items: TItem[], id: string):
+  number => (items.findIndex((item: TItem) => item.id === id));
+
+const removeItemAtIndex = <TItem>(array: TItem[], index: number):
+  TItem[] => ([...array.slice(0, index), ...array.slice(index + 1)]);
+
+const insertItemAtIndex = <TItem>(
+  array: TItem[], item: TItem, index: number):
+  TItem[] => ([...array.slice(0, index), item, ...array.slice(index)]);
+
+export const moveItem = <TItem>(
+  array: TItem[], from: number, to: number): TItem[] => {
+  const item = array[from];
+  return insertItemAtIndex(removeItemAtIndex(array, from), item, to);
+};
